71f1cb9bc8d5e773ce371046addf01d6edcb4117
dingqian <302965061@qq.com>
2016-04-06 19:20:39 +0800

    section-2/practice-3/new

--- pre-pos-master/practice/section-2/practice-3.js
+++ pre-pos-master/practice/section-2/practice-3.js
@@ -1,20 +1,74 @@
- function count_same_elements(collection) {
-    var re = [];
-  for(var i=0,v,l = collection.length; v = collection[i],i<l; i++)
-  {
-    //匹配数组中的字符串，匹配，返回字符串，否则，返回null
-    var rv = /^([a-z]+)(?:.+?(\d+))?/i.exec(v);
-    if (!re[rv[1]])
-        re[rv[1]] = 0;
-    /*
-    parseInt(string,radix)解析一个字符串，并返回一个数字
-    string为被解析的字符串，radix表示要解析的数字的基数,即几进制数。该值介于 2 ~ 36 之间。
-    如果省略该参数或其值为 0，则数字将以 10 为基础来解析。如果它以 “0x” 或 “0X” 开头，将以 16 为基数。
-    如果该参数小于 2 或者大于 36，则 parseInt() 将返回 NaN。  
-    */
-    re[rv[1]] += rv[2] ? parseInt(rv[2], 10) : 1;
-  }
-  return re;
+function count_same_elements(collection) {
+  //在这里写入代码
+  var new_collection = [
+      {name: "a", summary: 0},
+      {name:"e", summary: 0},
+      {name:"h", summary: 0},
+      {name:"t", summary: 0},
+      {name:"f", summary: 0},
+      {name:"c", summary: 0},
+      {name:"g", summary: 0},
+      {name:"b", summary: 0},
+      {name:"d", summary: 0}];
+   var count_new = counts(count_new,collection);
+      for (var j = 0; j < new_collection.length; j++)
+      {
+          new_collection[j].summary = count_new[j];
+      }
+      return new_collection;
 }
- 
- 
+
+function counts(count,collection_a) {
+    var count = [0,0,0,0,0,0,0,0,0];
+  for (var i = 0; i < collection_a.length; i++)
+  {
+        var a = 1;
+        if (collection_a[i].length != 1)
+        {
+            a = return_number(collection_a[i]);
+            collection_a[i] = return_char(collection_a[i]);
+        }
+        if (collection_a[i] === 'a')
+        count[0] +=a;
+        else if (collection_a[i] === 'e')
+        count[1] +=a;
+        else if (collection_a[i] === 'h')
+        count[2] +=a;
+        else if (collection_a[i] === 't')
+        count[3] +=a;
+        else if (collection_a[i] === 'f')
+        count[4] +=a;
+        else if (collection_a[i] === 'c')
+        count[5] +=a;
+        else if (collection_a[i] === 'g')
+        count[6] +=a;
+        else if (collection_a[i] === 'b')
+        count[7] +=a;
+        else if(collection_a[i] === 'd')
+        count[8] +=a;
+      
+  }
+  return count;
+}
+
+function return_char(collection) {
+    var string = [];
+    string = collection.split("");
+    var char = string[0];
+    return char;
+}
+
+function return_number(collection) {
+    var string = [];
+    var num = '';
+    var number;
+    string = collection.split("");
+    for (var i = 0; i < string.length; i++)
+    {
+        if(!isNaN(string[i]))
+        num += Math.floor(string[i]);
+    }
+    number = Number(num);
+    return number;
+}
+    
