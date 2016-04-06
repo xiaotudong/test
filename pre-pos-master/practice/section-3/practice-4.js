26057b4ca3d23024c502aa67d3692e29c0ce41d2
dingqian <302965061@qq.com>
2016-04-06 18:36:27 +0800

    section-3/practice-4/new

--- pre-pos-master/practice/section-3/practice-4.js
+++ pre-pos-master/practice/section-3/practice-4.js
@@ -44,25 +44,45 @@ function counts(count,collection_a) {
     var count = [0,0,0,0,0,0,0,0,0];
   for (var i = 0; i < collection_a.length; i++)
   {
-      if (collection_a[i] === 'a')
-      count[0]++;
-      else if (collection_a[i] === 'e')
-      count[1]++;
-      else if (collection_a[i] === 'h')
-      count[2]++;
-      else if (collection_a[i] === 't')
-      count[3]++;
-      else if (collection_a[i] === 'f')
-      count[4]++;
-      else if (collection_a[i] === 'c')
-      count[5]++;
-      else if (collection_a[i] === 'g')
-      count[6]++;
-      else if (collection_a[i] === 'b')
-      count[7]++;
-      else if(collection_a[i] === 'd-5')
-      count[8] += 5;
+        var a = 1;
+        if (collection_a[i].length != 1)
+        {
+            a = return_number(collection_a[i]);
+            collection_a[i] = return_char(collection_a[i]);
+        }
+        if (collection_a[i] === 'a')
+        count[0] +=a;
+        else if (collection_a[i] === 'e')
+        count[1] +=a;
+        else if (collection_a[i] === 'h')
+        count[2] +=a;
+        else if (collection_a[i] === 't')
+        count[3] +=a;
+        else if (collection_a[i] === 'f')
+        count[4] +=a;
+        else if (collection_a[i] === 'c')
+        count[5] +=a;
+        else if (collection_a[i] === 'g')
+        count[6] +=a;
+        else if (collection_a[i] === 'b')
+        count[7] +=a;
+        else if(collection_a[i] === 'd')
+        count[8] +=a;
+      
   }
   return count;
 }
-    
+
+function return_number(collection) {
+    var string = [];
+    string = collection.split("-");
+    var number = Math.floor(string[1]);
+    return number;
+}
+
+function return_char(collection) {
+    var string = [];
+    string = collection.split("-");
+    var char = string[0];
+    return char;
+}
